0758f4fd9a81f9ee7e06c7e5b8d06b675700d741
Abigail Rolling <arolling@gmail.com>
2016-04-08 10:24:55 -0700

    Create new user signup form

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    signUpNew(){
+      var params = {
+        username: this.get('usernameNew'),
+        zipcode: this.get('zipcodeNew') ? this.get('zipcodeNew') : '',
+        address: this.get('addressNew') ? this.get('addressNew') : ''
+      };
+      this.set('usernameNew', '');
+      this.set('zipcodeNew', '');
+      this.set('addressNew', '');
+      this.sendAction('newSignUp', params);
+    }
+  }
 });
