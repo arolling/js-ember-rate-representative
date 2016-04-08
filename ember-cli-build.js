6f88e413411b650127497984d160a2d37a341f9a
Abigail Rolling <arolling@gmail.com>
2016-04-08 08:28:40 -0700

    set up initial packages and dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('bower_components/moment/moment.js');
+  app.import('vendor/gmaps.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
